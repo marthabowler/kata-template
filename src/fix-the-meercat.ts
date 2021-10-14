4a4750a3d1624830a85d0e162959e1cb815f815c
Martha Bowler <mbowler133@gmail.com>
2021-10-14 15:59:28 +0100

    Add meerkat algorithm

---
+++ src/fix-the-meercat.ts
@@ -0,0 +1,12 @@
+/**
+ * This function will return the array in reverse order
+ * 
+ * @param {string[]} arr 
+ * @returns the array but in reverse order
+ */
+
+ function fixTheMeercat(arr: string[]) : string[] {
+    return arr.reverse();
+}
+
+export default fixTheMeercat;
