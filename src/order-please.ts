9df16e4240994a765b9e12f0cc46376efbd3bd26
Martha Bowler <mbowler133@gmail.com>
2021-10-15 15:42:47 +0100

    Add order please algorithm

---
+++ src/order-please.ts
@@ -0,0 +1,19 @@
+function order(words: string) : string {
+  let result = [];
+  let text = words.split(" ");
+
+  if (words === "") {
+    return "";
+  }
+
+  for (let i = 1; i < 10; i++) {
+    for (let j = 0; j < text.length; j++) {
+      if (text[j].includes(i.toString())) {
+        result.push(text[j]);
+      }
+    }
+  }
+let strResult = result.join(" ");
+return strResult;
+}
+export default order;
